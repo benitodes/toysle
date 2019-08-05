f2fc1d078b9dc272bba5a9b70a55e4b01d850523
jana meier <jana.meier@hotmail.de>
2019-08-05 19:34:33 +0800

    controllers done

---
+++ app/controllers/reviews_controller.rb
@@ -0,0 +1,18 @@
+class ReviewsController < ApplicationController
+  def create
+    @booking = Booking.find(params[:booking_id])
+    @review = Review.new(review_params)
+    @review.booking = @booking
+    if @review.save
+      redirect_to dashboard_path
+    else
+      render 'bookings/show'
+    end
+  end
+
+  private
+
+  def review_params
+    params.require(:review).permit(:content, :rating)
+  end
+end
---
+++ app/controllers/toys_controller.rb
@@ -0,0 +1,46 @@
+class ToysController < ApplicationController
+  before_action :set_toy, only: [:show, :edit, :update, :destroy]
+
+  def index
+    @toys = Toy.all
+  end
+
+  def show
+  end
+
+  def new
+    @toy = Toy.new
+  end
+
+  def create
+    @user = current_user
+    @toy = Toy.new(toy_params)
+    @toy.user = @user
+    if @toy.save
+      redirect_to dashboard_path
+    else
+      render "pages/dashboard"
+    end
+  end
+
+  def edit
+  end
+
+  def update
+    @toy.update(toy_params)
+  end
+
+  def destroy
+    @toy.destroy
+    redirect
+  end
+
+  def set_toy
+    @toy = Toy.find(params[:id])
+  end
+
+  def toy_params
+    params.require(:toy).permit(:name, :description, :address, :daily_price, :max_age, :theme_id)
+  end
+
+end
