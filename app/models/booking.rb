d69b917c8cecdc71c50a9151b9e5b7b347b55c7a
jana meier <jana.meier@hotmail.de>
2019-08-06 16:22:24 +0800

    validations added

--- app/models/booking.rb
+++ app/models/booking.rb
@@ -1,4 +1,8 @@
 class Booking < ApplicationRecord
   belongs_to :toy
   belongs_to :user
+  # validations
+  validates :start_date, presence: true
+  validates :end_date, presence: true
+  validates :total_price, presence: true
 end
--- app/models/review.rb
+++ app/models/review.rb
@@ -1,3 +1,6 @@
 class Review < ApplicationRecord
   belongs_to :booking
+  # validations:
+  validates :content, presence: true, length: { minimum: 60 }
+  validates :rating, presence: true
 end
