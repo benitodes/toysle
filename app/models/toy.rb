6fcc3df8b6bb09cb23cc447568348e33c42d8fef
Samba Diaw <sambachimerediaw@gmail.com>
2019-08-07 16:56:13 +0800

    map added to our index

--- app/models/toy.rb
+++ app/models/toy.rb
@@ -8,6 +8,7 @@ class Toy < ApplicationRecord
   validates :description, presence: true, length: { minimum: 60 }
   validates :daily_price, presence: true
   validates :address, presence: true
+  # geocode:
   geocoded_by :address
   after_validation :geocode, if: :will_save_change_to_address?
 end
--- config/initializers/geocoder.rb
+++ config/initializers/geocoder.rb
@@ -1,3 +1,5 @@
+# rails generate geocoder:config
+
 Geocoder.configure(
   # Geocoding options
   # timeout: 3,                 # geocoding service timeout (secs)
