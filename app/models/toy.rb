4dc95fe104ea8f34ccb9f7abbb649eb1869119d1
Samba Diaw <sambachimerediaw@gmail.com>
2019-08-07 12:53:54 +0800

    add column longitude and latitude into my toy model

--- app/models/toy.rb
+++ app/models/toy.rb
@@ -8,4 +8,6 @@ class Toy < ApplicationRecord
   validates :description, presence: true, length: { minimum: 60 }
   validates :daily_price, presence: true
   validates :address, presence: true
+  geocoded_by :address
+  after_validation :geocode, if: :will_save_change_to_address?
 end
