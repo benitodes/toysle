52796e357b262fbe534a3915ef7bdac1452e10ea
jana meier <jana.meier@hotmail.de>
2019-08-07 12:56:27 +0800

    added method to toy model and created custom validation

--- app/models/toy.rb
+++ app/models/toy.rb
@@ -8,4 +8,17 @@ class Toy < ApplicationRecord
   validates :description, presence: true, length: { minimum: 60 }
   validates :daily_price, presence: true
   validates :address, presence: true
+
+  # **Flatpickr** will plug into each of the date fields to display a nice date-picker, but before we do that,
+  #   we need our backend to send to the front-end a list of all the **unavailable dates**.
+  #   Accordingly to **flatpickr**’s documentation, it is possible to pass an **option** to **disable** dates
+  #   as an array of ranges of dates `[{ from: "2018-05-01, to: "2018-05-20"}]`.
+  #   Let’s add a method `#unavailable_dates` to our Toy model to do just that:
+
+  def unavailable_dates
+    bookings.pluck(:start_date, :end_date).map do |range|
+      { from: range[0], to: range[1] }
+    end
+  end
+
 end
