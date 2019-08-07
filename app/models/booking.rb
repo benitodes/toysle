3072d62ec9afd5ea2f207ee6a990cf4c1387b654
jana meier <jana.meier@hotmail.de>
2019-08-07 12:00:10 +0800

    created dependencies

--- app/models/booking.rb
+++ app/models/booking.rb
@@ -1,6 +1,7 @@
 class Booking < ApplicationRecord
   belongs_to :toy
   belongs_to :user
+  has_one :review, dependent: :destroy
   # validations
   validates :start_date, presence: true
   validates :end_date, presence: true
