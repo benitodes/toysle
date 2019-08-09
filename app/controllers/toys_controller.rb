65337822ef3243f90ca29b73a859399e7a4afd01
jana meier <jana.meier@hotmail.de>
2019-08-09 13:17:58 +0800

    price slider filters toys successfully

--- app/controllers/toys_controller.rb
+++ app/controllers/toys_controller.rb
@@ -14,12 +14,12 @@ class ToysController < ApplicationController
       # fetch min age from age slider
       min_age_input = params[:slider][:age_input].to_i
       # fetch min and max price from price slider
-      min_price_input = params[:slider][:price_input][0].to_i
-      max_price_input = params[:slider][:price_input][1].to_i
+      min_price_input = params[:slider][:price_input].split(',')[0].to_i
+      max_price_input = params[:slider][:price_input].split(',')[1].to_i
       # only show toys that match filter
       @toys = @toys.select { |t| t.min_age >= min_age_input && t.daily_price > min_price_input }
+      # raise
     end
-    raise
   end
 
   def show
